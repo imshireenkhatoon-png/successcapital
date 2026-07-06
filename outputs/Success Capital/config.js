7c33669bdd9152166de2394ac09e90facfead51d
imshireenkhatoon <imshireenkhatoon@gmail.com>
2026-07-06 17:48:27 +0530

    Updated website

--- outputs/Success Capital/config.js
+++ outputs/Success Capital/config.js
@@ -1,1 +1,1 @@
-const SCRIPT_URL = "AKfycbw5_kFKiEmhizTG5TQmr6vp_smM_nj3WDEOXYuRlWw9QV0JSnnQwerCjsyko4t3kiCIjg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw5_kFKiEmhizTG5TQmr6vp_smM_nj3WDEOXYuRlWw9QV0JSnnQwerCjsyko4t3kiCIjg/exec";
